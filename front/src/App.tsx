c74b2c94add35c1f670bf3055e97720ace02dcc5
Alexis Güere <freealex2.0@gmail.com>
2023-05-15 16:47:06 -0400

    feat(front): consume api in the main component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import { getCommits } from './api'
 
 function App() {
+  
+  useEffect(()=>{
+    getCommits()
+      .then(data => {
+        console.log(data);
+      })
+  },[]);
 
   return (
     <>
